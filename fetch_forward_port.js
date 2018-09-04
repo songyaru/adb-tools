4e109203496543b52c2a071fd5a656fc97b7fef8
songyaru <songyaru9@gmail.com>
2018-09-04 20:20:22 +0800

    增加 远程调试协议版本获取

--- fetch_forward_port.js
+++ fetch_forward_port.js
@@ -2,20 +2,28 @@ const rp = require('request-promise');
 
 const fetchForwardPort = ({url = 'localhost', port = 4000, filter = data => data} = {}) => {
     return new Promise((resolve, reject) => {
-        return rp('http://' + url + ':' + port + '/json').then(data => {
-            let result = JSON.parse(data);
-            if (result) {
-                let json = filter(result);
-                if (json && json.length) {
-                    return resolve({json, port});
+        return rp('http://' + url + ':' + port + '/json')
+            .then(data => {
+                let result = JSON.parse(data);
+                if (result) {
+                    let json = filter(result);
+                    if (json && json.length) {
+                        return {json, port};
+                    }
                 }
-            }
 
-            reject('http://' + url + ':' + port + '/json 没有返回有效的数据');
+                reject('http://' + url + ':' + port + '/json 没有返回有效的数据');
 
-        }).catch(error => {
-            reject(error);
-        });
+            })
+            .then(ret => {
+                return rp('http://' + url + ':' + port + '/json/version').then(data => {
+                    ret.version = JSON.parse(data) || {};
+                    resolve(ret);
+                });
+            })
+            .catch(error => {
+                reject(error);
+            });
     });
 };
 
